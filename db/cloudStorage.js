afe0c9e3e4f7066cb1c7111db5f0609d6a04d30d
Rapong <gnopgnopar@gmail.com>
2023-12-13 09:23:35 +0700

    add comments

--- db/cloudStorage.js
+++ db/cloudStorage.js
@@ -2,11 +2,12 @@ const { Storage } = require('@google-cloud/storage');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// diisi sesuai GCS yang ada di google cloud console, untuk key nya bisa membuat di service account
 const storage = new Storage({
   projectId: 'project-rafie-1',
   key: './storageKey.json',
 });
 
-const bucket = storage.bucket('photo_exam_try');
+const bucket = storage.bucket('photo_exam_try');  // diisi sesuai nama bucket yang dipakai
 
 module.exports = { bucket, storage };
