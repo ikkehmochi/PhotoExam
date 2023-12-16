76964ac4421dd4945ae1fcb122072edb32c432c4
Rapong <gnopgnopar@gmail.com>
2023-12-16 23:53:15 +0700

    update service account

--- db/cloudStorage.js
+++ db/cloudStorage.js
@@ -5,9 +5,12 @@ dotenv.config();
 // diisi sesuai GCS yang ada di google cloud console, untuk key nya bisa membuat di service account
 const storage = new Storage({
   projectId: 'project-rafie-1',
-  key: './storageKey.json',
+  credentials: {
+    client_email: process.env.GCS_CLIENT_EMAIL,
+    private_key: process.env.GCS_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+  },
 });
 
-const bucket = storage.bucket('photo_exam_try');  // diisi sesuai nama bucket yang dipakai
+const bucket = storage.bucket('photo_exam_try'); // diisi sesuai nama bucket yang dipakai
 
 module.exports = { bucket, storage };
