58a40e221d8f06d4f77f8ae379e9f0e518c2a875
ChiristianK <91868266+ChiristianK@users.noreply.github.com>
2023-12-18 15:47:59 +0700

    Add files via upload
    handwritting recognition model using google cloud

---
+++ handwritten_recognition.py
@@ -0,0 +1,38 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+import pandas as pd 
+
+
+# client 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'PhotoExam.json'
+client = vision.ImageAnnotatorClient()
+
+image_path = './WhatsApp Image 2023-12-14 at 22.49.30.jpeg'
+with io.open(image_path, 'rb') as image_file:
+    content = image_file.read()
+image = vision.Image(content=content)
+
+response = client.document_text_detection(image=image)
+
+for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print(f"\nBlock confidence: {block.confidence}\n")
+
+            for paragraph in block.paragraphs:
+                print("Paragraph confidence: {}".format(paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = "".join([symbol.text for symbol in word.symbols])
+                    print(
+                        "Word text: {} (confidence: {})".format(
+                            word_text, word.confidence
+                        )
+                    )
+
+                    for symbol in word.symbols:
+                        print(
+                            "\tSymbol: {} (confidence: {})".format(
+                                symbol.text, symbol.confidence
+                            )
+                        )
