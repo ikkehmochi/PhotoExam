8b1cd80e275b47525cff0cb1bd03a30fd3a69a06
ChiristianK <91868266+ChiristianK@users.noreply.github.com>
2023-12-18 17:40:14 +0700

    Update handwritten_recognition.py
    change the confidence line code to comment line code, adding line for handwritten image to text detection

--- handwritten_recognition.py
+++ handwritten_recognition.py
@@ -1,6 +1,6 @@
 import os, io
 from google.cloud import vision
-from google.cloud.vision import types
+from google.cloud.vision_v1 import types
 import pandas as pd 
 
 
@@ -8,31 +8,37 @@ import pandas as pd
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'PhotoExam.json'
 client = vision.ImageAnnotatorClient()
 
+# reading image
 image_path = './WhatsApp Image 2023-12-14 at 22.49.30.jpeg'
 with io.open(image_path, 'rb') as image_file:
     content = image_file.read()
 image = vision.Image(content=content)
 
+# converting image to text
 response = client.document_text_detection(image=image)
+full_txt = response.full_text_annotation.text
+print(full_txt)
 
-for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            print(f"\nBlock confidence: {block.confidence}\n")
 
-            for paragraph in block.paragraphs:
-                print("Paragraph confidence: {}".format(paragraph.confidence))
+# # check for each word confidence level
+# for page in response.full_text_annotation.pages:
+#         for block in page.blocks:
+#             print(f"\nBlock confidence: {block.confidence}\n")
 
-                for word in paragraph.words:
-                    word_text = "".join([symbol.text for symbol in word.symbols])
-                    print(
-                        "Word text: {} (confidence: {})".format(
-                            word_text, word.confidence
-                        )
-                    )
+#             for paragraph in block.paragraphs:
+#                 print("Paragraph confidence: {}".format(paragraph.confidence))
 
-                    for symbol in word.symbols:
-                        print(
-                            "\tSymbol: {} (confidence: {})".format(
-                                symbol.text, symbol.confidence
-                            )
-                        )
+#                 for word in paragraph.words:
+#                     word_text = "".join([symbol.text for symbol in word.symbols])
+#                     print(
+#                         "Word text: {} (confidence: {})".format(
+#                             word_text, word.confidence
+#                         )
+#                     )
+
+#                     for symbol in word.symbols:
+#                         print(
+#                             "\tSymbol: {} (confidence: {})".format(
+#                                 symbol.text, symbol.confidence
+#                             )
+#                         )
